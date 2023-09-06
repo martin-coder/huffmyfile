fac1d3c7e0385d4ee73498095d165695fc940c09
Martin Coder <martincoder1@gmail.com>
2023-09-05 22:47:00 -0700

    Add license header to bitreader.go

--- pkg/bitreader.go
+++ pkg/bitreader.go
@@ -1,3 +1,11 @@
+/*
+Copyright © 2023 Martin Coder <martincoder1@gmail.com>
+
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file or at
+https://opensource.org/licenses/MIT.
+*/
+
 package huffmyfile
 
 import (
